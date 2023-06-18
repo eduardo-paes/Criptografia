3d29044d3f389ff3140e3ae5c0a42f9d658c7859
PSThiago <thiago.paes.silva@hotmail.com>
2023-06-17 22:33:50 -0300

    add generate bias table function.

--- task2/task2.c
+++ task2/task2.c
@@ -4,8 +4,10 @@
 #include <stdint.h>
 #include <unistd.h>
 
-const int NUM_BYTES = 4;
-const unsigned char SBOX[256] = {41, 157, 143, 131, 222, 227, 111, 16, 81, 29, 204, 182, 77, 142, 212, 165, 152, 72, 162, 193, 132, 207, 49, 63, 154, 247, 134, 45, 119, 55, 252, 236, 8, 93, 85, 48, 36, 180, 87, 113, 175, 116, 14, 213, 88, 106, 195, 135, 253, 27, 231, 203, 192, 100, 20, 164, 217, 189, 80, 82, 90, 127, 199, 149, 25, 170, 245, 206, 183, 103, 220, 233, 109, 173, 94, 210, 158, 229, 221, 226, 120, 13, 21, 79, 235, 24, 248, 118, 26, 174, 140, 125, 156, 12, 196, 23, 130, 230, 3, 58, 169, 96, 108, 244, 102, 249, 215, 237, 62, 148, 114, 123, 214, 65, 84, 159, 179, 105, 40, 167, 64, 54, 9, 201, 19, 97, 52, 22, 32, 53, 70, 98, 139, 68, 59, 163, 15, 202, 47, 228, 6, 151, 251, 181, 243, 5, 188, 209, 224, 57, 250, 240, 95, 178, 17, 208, 110, 197, 60, 255, 71, 239, 91, 66, 128, 186, 112, 30, 28, 211, 242, 37, 46, 69, 115, 107, 177, 234, 83, 38, 161, 76, 171, 61, 75, 194, 43, 99, 35, 67, 101, 122, 50, 89, 144, 184, 198, 33, 126, 136, 73, 216, 141, 92, 219, 176, 150, 11, 185, 138, 145, 155, 146, 168, 51, 172, 160, 39, 225, 78, 200, 10, 133, 74, 2, 190, 232, 86, 147, 223, 104, 137, 191, 218, 44, 121, 34, 117, 246, 31, 241, 187, 1, 0, 18, 7, 166, 153, 205, 42, 129, 56, 254, 238, 4, 124};
+#define SBOX_SIZE 256
+#define NUM_BYTES 4
+
+const unsigned char SBOX[SBOX_SIZE] = {41, 157, 143, 131, 222, 227, 111, 16, 81, 29, 204, 182, 77, 142, 212, 165, 152, 72, 162, 193, 132, 207, 49, 63, 154, 247, 134, 45, 119, 55, 252, 236, 8, 93, 85, 48, 36, 180, 87, 113, 175, 116, 14, 213, 88, 106, 195, 135, 253, 27, 231, 203, 192, 100, 20, 164, 217, 189, 80, 82, 90, 127, 199, 149, 25, 170, 245, 206, 183, 103, 220, 233, 109, 173, 94, 210, 158, 229, 221, 226, 120, 13, 21, 79, 235, 24, 248, 118, 26, 174, 140, 125, 156, 12, 196, 23, 130, 230, 3, 58, 169, 96, 108, 244, 102, 249, 215, 237, 62, 148, 114, 123, 214, 65, 84, 159, 179, 105, 40, 167, 64, 54, 9, 201, 19, 97, 52, 22, 32, 53, 70, 98, 139, 68, 59, 163, 15, 202, 47, 228, 6, 151, 251, 181, 243, 5, 188, 209, 224, 57, 250, 240, 95, 178, 17, 208, 110, 197, 60, 255, 71, 239, 91, 66, 128, 186, 112, 30, 28, 211, 242, 37, 46, 69, 115, 107, 177, 234, 83, 38, 161, 76, 171, 61, 75, 194, 43, 99, 35, 67, 101, 122, 50, 89, 144, 184, 198, 33, 126, 136, 73, 216, 141, 92, 219, 176, 150, 11, 185, 138, 145, 155, 146, 168, 51, 172, 160, 39, 225, 78, 200, 10, 133, 74, 2, 190, 232, 86, 147, 223, 104, 137, 191, 218, 44, 121, 34, 117, 246, 31, 241, 187, 1, 0, 18, 7, 166, 153, 205, 42, 129, 56, 254, 238, 4, 124};
 
 // Retorna o menor entre dois números
 int minValue(int a, int b)
@@ -13,6 +15,22 @@ int minValue(int a, int b)
     if (a < b)
         return a;
     return b;
+}
+
+// Retorna o iésimo bit de x
+unsigned char bit( unsigned char x, unsigned char i ) {
+    return (x >> (8 - i)) & 1;
+}
+
+float ** allocTable(int size){
+    float ** newTable = malloc(size * sizeof(float *));
+    for(int i = 0; i < size; ++i) newTable[i] = malloc(size * sizeof(float)); 
+    return newTable;
+}
+
+void deallocTable(float ** table, int size){
+    for(int i = 0; i < size; ++i) free(table[i]); 
+    free(table);
 }
 
 // Obtém password do usuário através da linha de comando
@@ -63,28 +81,34 @@ uint8_t countSetBits(uint8_t n) {
 }
 
 // Gera a tabela de bias
-void generateBiasTable() {
-    printf("Bias Table for SBOX (8 x 8)\n");
-    uint8_t diffX, diffY, diff;
-    
-    // Calcula a diferença entre x e y antes e depois da aplicação da SBOX
-    for (int x = 0; x < 8; x++)
-    {
-        for (int y = 0; y < 8; y++)
-        {
-            diffX = x ^ y;
-            diffY = SBOX[x] ^ SBOX[y];
-            diff = diffX ^ diffY;
-            printf("%d\t", countSetBits(diff));
+void generateBiasTable(float **biasTable) {
+    uchar bitValues[SBOX_SIZE][8];
+    for (int i = 0; i < SBOX_SIZE; i++) {
+        for (int j = 0; j < 8; j++) {
+            bitValues[i][j] = bit(i, j + 1);
         }
-        printf("\n");
+    }
+
+    for (int x = 0; x < SBOX_SIZE; x++) {
+        for (int y = 0; y < SBOX_SIZE; y++) {
+            int matches = 0;
+            for (int X = 0; X < SBOX_SIZE; X++) {
+                int a = 0;
+                int b = 0;
+                for (int i = 0; i < 8; i++) {
+                    a ^= bitValues[X][i] & bitValues[x][i];
+                    b ^= bitValues[SBOX[X]][i] & bitValues[y][i];
+                }
+                if (a == b) {
+                    matches++;
+                }
+            }
+            biasTable[x][y] = matches - SBOX_SIZE / 2;
+        }
     }
 }
 
 int main (int argc, char* argv[]) {
-
-    // Gera a tabela de bias
-    generateBiasTable();
 
     // Verifica se o número de argumentos é válido
     if (argc < 2)
@@ -92,6 +116,12 @@ int main (int argc, char* argv[]) {
         printf("Message is required for encryptation.\n");
         return 1;
     }
+
+    float ** biasTable = allocTable(SBOX_SIZE);
+    
+    generateBiasTable(biasTable);
+
+    deallocTable(biasTable, SBOX_SIZE);
 
     // Obtém a mensagem a ser encriptada
     char* message = concatArguments(argc, argv);
